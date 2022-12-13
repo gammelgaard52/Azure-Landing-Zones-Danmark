64bec23e959093ed2cf2ee31c4588591ea63e7bb
Martin Gammelgaard Rasmussen <gammelgaard_mink@msn.com>
2022-12-13 09:36:12 +0100

    Adding validation if Bicep files

---
+++ scripts/Test-Bicep.Tests.ps1
@@ -0,0 +1,14 @@
+Describe "Test-Bicep" {
+    $path = Resolve-Path "$PSScriptRoot/.."
+
+    $testCases = Get-ChildItem -Path $path -Include *.bicep -Recurse | ForEach-Object {
+        @{
+            Name     = $PSItem.FullName.Replace($path, "")
+            FullName = $PSItem.FullName
+        }
+    }
+
+    It "<Name> is valid bicep" -TestCases $testCases {
+        Test-Bicep -Path (Get-Content -Path $FullName) | Should -Be $true
+    }
+}
